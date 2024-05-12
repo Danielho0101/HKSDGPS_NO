3de93df4f89b7765fc97015f981d5161e16c0ab6
Daniel <lsps08@yahoo.com.hk>
2024-05-12 23:10:03 +0800

    all

--- README.md
+++
@@ -1,1 +0,0 @@
-# HKSDGPS_NO
---
+++ script.js
@@ -0,0 +1,470 @@
+let btn1a = document.getElementById('btn1a');
+let btnOrange1a = btn1a.style.background = "orange";
+let i1a = 0;
+
+btn1a.addEventListener("click", function onClick(){
+    i1a++;
+    if ( i1a%2 == 0){
+    btn1a.style.background = "orange";
+    btn1a.style.color = "white";
+    btn1a.style.borderColor = "red";
+    btn1a.style.fontSize = "1000px";
+    btn1a.classList.remove('scale-up-center');
+
+} else if( i1a%2 == !0){
+    btn1a.style.background = "rgb(230, 204, 255)";
+    btn1a.style.color = "purple";
+    btn1a.style.borderColor = "rgb(145, 107, 242)";
+    btn1a.classList.add('scale-up-center');
+}
+
+});
+
+const btn2a = document.getElementById('btn2a');
+const btnOrange2a = btn2a.style.background = "orange";
+let i2a = 0;
+
+btn2a.addEventListener("click", function onClick(){
+    i2a++;
+    if ( i2a%2 == 0){
+    btn2a.style.background = "orange";
+    btn2a.style.color = "white";
+    btn2a.style.borderColor = "red";
+    btn2a.classList.remove('scale-up-center');
+} else if( i2a%2 == !0){
+    btn2a.style.background = "rgb(230, 204, 255)";
+    btn2a.style.color = "purple";
+    btn2a.style.borderColor = "rgb(145, 107, 242)";
+    btn2a.classList.add('scale-up-center');
+}
+
+});
+const btn3a = document.getElementById('btn3a');
+const btnOrange3a = btn3a.style.background = "orange";
+let i3a = 0;
+
+btn3a.addEventListener("click", function onClick(){
+    i3a++;
+    if ( i3a%2 == 0){
+    btn3a.style.background = "orange";
+    btn3a.style.color = "white";
+    btn3a.style.borderColor = "red";
+    btn3a.classList.remove('scale-up-center');
+
+} else if( i3a%2 == !0){
+    btn3a.style.background = "rgb(230, 204, 255)";
+    btn3a.style.color = "purple";
+    btn3a.style.borderColor = "rgb(145, 107, 242)";
+    btn3a.classList.add('scale-up-center');
+}
+});
+
+const btn4a = document.getElementById('btn4a');
+const btnOrange4a = btn4a.style.background = "orange";
+let i4a = 0;
+
+btn4a.addEventListener("click", function onClick(){
+    i4a++;
+    if ( i4a%2 == 0){
+    btn4a.style.background = "orange";
+    btn4a.style.color = "white";
+    btn4a.style.borderColor = "red";
+    btn4a.classList.remove('scale-up-center');
+
+} else if( i4a%2 == !0){
+    btn4a.style.background = "rgb(230, 204, 255)";
+    btn4a.style.color = "purple";
+    btn4a.style.borderColor = "rgb(145, 107, 242)";
+    btn4a.classList.add('scale-up-center');  
+}
+});
+
+const btn5a = document.getElementById('btn5a');
+const btnOrange5a = btn5a.style.background = "orange";
+let i5a = 0;
+
+btn5a.addEventListener("click", function onClick(){
+    i5a++;
+    if ( i5a%2 == 0){
+    btn5a.style.background = "orange";
+    btn5a.style.color = "white";
+    btn5a.style.borderColor = "red";
+    btn5a.classList.remove('scale-up-center');
+} else if( i5a%2 == !0){
+    btn5a.style.background = "rgb(230, 204, 255)";
+    btn5a.style.color = "purple";
+    btn5a.style.borderColor = "rgb(145, 107, 242)";
+    btn5a.classList.add('scale-up-center');
+}
+});
+
+const btn6a = document.getElementById('btn6a');
+const btnOrange6a = btn6a.style.background = "orange";
+let i6a = 0;
+
+btn6a.addEventListener("click", function onClick(){
+    i6a++;
+    if ( i6a%2 == 0){
+    btn6a.style.background = "orange";
+    btn6a.style.color = "white";
+    btn6a.style.borderColor = "red";
+    btn6a.classList.remove('scale-up-center');
+} else if( i6a%2 == !0){
+    btn6a.style.background = "rgb(230, 204, 255)";
+    btn6a.style.color = "purple";
+    btn6a.style.borderColor = "rgb(145, 107, 242)";
+    btn6a.classList.add('scale-up-center');
+}
+
+});
+
+// Class B
+const btn1b = document.getElementById('btn1b');
+const btnOrange1b = btn1b.style.background = "orange";
+let i1b = 0;
+
+btn1b.addEventListener("click", function onClick(){
+    i1b++;
+    if ( i1b%2 == 0){
+    btn1b.style.background = "orange";
+    btn1b.style.color = "white";
+    btn1b.style.borderColor = "red";
+    btn1b.classList.remove('scale-up-center');
+} else if( i1b%2 == !0){
+    btn1b.style.background = "rgb(230, 204, 255)";
+    btn1b.style.color = "purple";
+    btn1b.style.borderColor = "rgb(145, 107, 242)";
+    btn1b.classList.add('scale-up-center');
+}
+});
+
+const btn2b = document.getElementById('btn2b');
+const btnOrange2b = btn2b.style.background = "orange";
+let i2b = 0;
+
+btn2b.addEventListener("click", function onClick(){
+    i2b++;
+    if ( i2b%2 == 0){
+    btn2b.style.background = "orange";
+    btn2b.style.color = "white";
+    btn2b.style.borderColor = "red";
+    btn2b.classList.remove('scale-up-center');
+} else if( i2b%2 == !0){
+    btn2b.style.background = "rgb(230, 204, 255)";
+    btn2b.style.color = "purple";
+    btn2b.style.borderColor = "rgb(145, 107, 242)";
+    btn2b.classList.add('scale-up-center');
+}
+});
+
+const btn3b = document.getElementById('btn3b');
+const btnOrange3b = btn3b.style.background = "orange";
+let i3b = 0;
+
+btn3b.addEventListener("click", function onClick(){
+    i3b++;
+    if ( i3b%2 == 0){
+    btn3b.style.background = "orange";
+    btn3b.style.color = "white";
+    btn3b.style.borderColor = "red";
+    btn3b.classList.remove('scale-up-center');
+} else if( i3b%2 == !0){
+    btn3b.style.background = "rgb(230, 204, 255)";
+    btn3b.style.color = "purple";
+    btn3b.style.borderColor = "rgb(145, 107, 242)";
+    btn3b.classList.add('scale-up-center');
+}
+});
+
+const btn4b = document.getElementById('btn4b');
+const btnOrange4b = btn4b.style.background = "orange";
+let i4b = 0;
+
+btn4b.addEventListener("click", function onClick(){
+    i4b++;
+    if ( i4b%2 == 0){
+    btn4b.style.background = "orange";
+    btn4b.style.color = "white";
+    btn4b.style.borderColor = "red";
+    btn4b.classList.remove('scale-up-center');
+
+} else if( i4b%2 == !0){
+    btn4b.style.background = "rgb(230, 204, 255)";
+    btn4b.style.color = "purple";
+    btn4b.style.borderColor = "rgb(145, 107, 242)";
+    btn4b.classList.add('scale-up-center');
+}
+});
+
+const btn5b = document.getElementById('btn5b');
+const btnOrange5b = btn5b.style.background = "orange";
+let i5b = 0;
+
+btn5b.addEventListener("click", function onClick(){
+    i5b++;
+    if ( i5b%2 == 0){
+    btn5b.style.background = "orange";
+    btn5b.style.color = "white";
+    btn5b.style.borderColor = "red";
+    btn5b.classList.remove('scale-up-center');
+} else if( i5b%2 == !0){
+    btn5b.style.background = "rgb(230, 204, 255)";
+    btn5b.style.color = "purple";
+    btn5b.style.borderColor = "rgb(145, 107, 242)";
+    btn5b.classList.add('scale-up-center');
+}
+});
+
+const btn6b = document.getElementById('btn6b');
+const btnOrange6b = btn6b.style.background = "orange";
+let i6b = 0;
+
+btn6b.addEventListener("click", function onClick(){
+    i6b++;
+    if ( i6b%2 == 0){
+    btn6b.style.background = "orange";
+    btn6b.style.color = "white";
+    btn6b.style.borderColor = "red";
+    btn6b.classList.remove('scale-up-center');
+} else if( i6b%2 == !0){
+    btn6b.style.background = "rgb(230, 204, 255)";
+    btn6b.style.color = "purple";
+    btn6b.style.borderColor = "rgb(145, 107, 242)";
+    btn6b.classList.add('scale-up-center');
+}
+});
+
+// Class C
+const btn1c = document.getElementById('btn1c');
+const btnOrange1c = btn1c.style.background = "orange";
+let i1c = 0;
+
+btn1c.addEventListener("click", function onClick(){
+    i1c++;
+    if ( i1c%2 == 0){
+    btn1c.style.background = "orange";
+    btn1c.style.color = "white";
+    btn1c.style.borderColor = "red";
+    btn1c.classList.remove('scale-up-center');
+} else if( i1c%2 == !0){
+    btn1c.style.background = "rgb(230, 204, 255)";
+    btn1c.style.color = "purple";
+    btn1c.style.borderColor = "rgb(145, 107, 242)";
+    btn1c.classList.add('scale-up-center');
+}
+
+});
+
+const btn2c = document.getElementById('btn2c');
+const btnOrange2c = btn2c.style.background = "orange";
+let i2c = 0;
+
+btn2c.addEventListener("click", function onClick(){
+    i2c++;
+    if ( i2c%2 == 0){
+    btn2c.style.background = "orange";
+    btn2c.style.color = "white";
+    btn2c.style.borderColor = "red";
+    btn2c.classList.remove('scale-up-center');
+
+} else if( i2c%2 == !0){
+    btn2c.style.background = "rgb(230, 204, 255)";
+    btn2c.style.color = "purple";
+    btn2c.style.borderColor = "rgb(145, 107, 242)";
+    btn2c.classList.add('scale-up-center');
+}
+});
+
+const btn3c = document.getElementById('btn3c');
+const btnOrange3c = btn3c.style.background = "orange";
+let i3c = 0;
+
+btn3c.addEventListener("click", function onClick(){
+    i3c++;
+    if ( i3c%2 == 0){
+    btn3c.style.background = "orange";
+    btn3c.style.color = "white";
+    btn3c.style.borderColor = "red";
+    btn3c.classList.remove('scale-up-center');
+
+} else if( i3c%2 == !0){
+    btn3c.style.background = "rgb(230, 204, 255)";
+    btn3c.style.color = "purple";
+    btn3c.style.borderColor = "rgb(145, 107, 242)";
+    btn3c.classList.add('scale-up-center');
+}
+});
+
+const btn4c = document.getElementById('btn4c');
+const btnOrange4c = btn4c.style.background = "orange";
+let i4c = 0;
+
+btn4c.addEventListener("click", function onClick(){
+    i4c++;
+    if ( i4c%2 == 0){
+    btn4c.style.background = "orange";
+    btn4c.style.color = "white";
+    btn4c.style.borderColor = "red";
+    btn4c.classList.remove('scale-up-center');
+} else if( i4c%2 == !0){
+    btn4c.style.background = "rgb(230, 204, 255)";
+    btn4c.style.color = "purple";
+    btn4c.style.borderColor = "rgb(145, 107, 242)";
+    btn4c.classList.add('scale-up-center');
+}
+});
+
+const btn5c = document.getElementById('btn5c');
+const btnOrange5c = btn5c.style.background = "orange";
+let i5c = 0;
+
+btn5c.addEventListener("click", function onClick(){
+    i5c++;
+    if ( i5c%2 == 0){
+    btn5c.style.background = "orange";
+    btn5c.style.color = "white";
+    btn5c.style.borderColor = "red";
+    btn5c.classList.remove('scale-up-center');
+} else if( i5c%2 == !0){
+    btn5c.style.background = "rgb(230, 204, 255)";
+    btn5c.style.color = "purple";
+    btn5c.style.borderColor = "rgb(145, 107, 242)";
+    btn5c.classList.add('scale-up-center');
+}
+});
+
+const btn6c = document.getElementById('btn6c');
+const btnOrange6c = btn6c.style.background = "orange";
+let i6c = 0;
+
+btn6c.addEventListener("click", function onClick(){
+    i6c++;
+    if ( i6c%2 == 0){
+    btn6c.style.background = "orange";
+    btn6c.style.color = "white";
+    btn6c.style.borderColor = "red";
+    btn6c.classList.remove('scale-up-center');
+
+} else if( i6c%2 == !0){
+    btn6c.style.background = "rgb(230, 204, 255)";
+    btn6c.style.color = "purple";
+    btn6c.style.borderColor = "rgb(145, 107, 242)";
+    btn6c.classList.add('scale-up-center');
+}
+});
+
+// Class D
+const btn1d = document.getElementById('btn1d');
+const btnOrange1d = btn1d.style.background = "orange";
+let i1d = 0;
+
+btn1d.addEventListener("click", function onClick(){
+    i1d++;
+    if ( i1d%2 == 0){
+    btn1d.style.background = "orange";
+    btn1d.style.color = "white";
+    btn1d.style.borderColor = "red";
+    btn1d.classList.remove('scale-up-center');
+} else if( i1d%2 == !0){
+    btn1d.style.background = "rgb(230, 204, 255)";
+    btn1d.style.color = "purple";
+    btn1d.style.borderColor = "rgb(145, 107, 242)";
+    btn1d.classList.add('scale-up-center');
+}
+
+});
+
+const btn2d = document.getElementById('btn2d');
+const btnOrange2d = btn2d.style.background = "orange";
+let i2d = 0;
+
+btn2d.addEventListener("click", function onClick(){
+    i2d++;
+    if ( i2d%2 == 0){
+    btn2d.style.background = "orange";
+    btn2d.style.color = "white";
+    btn2d.style.borderColor = "red";
+    btn2d.classList.remove('scale-up-center');
+} else if( i2d%2 == !0){
+    btn2d.style.background = "rgb(230, 204, 255)";
+    btn2d.style.color = "purple";
+    btn2d.style.borderColor = "rgb(145, 107, 242)";
+    btn2d.classList.add('scale-up-center');
+}
+});
+
+const btn3d = document.getElementById('btn3d');
+const btnOrange3d = btn3d.style.background = "orange";
+let i3d = 0;
+
+btn3d.addEventListener("click", function onClick(){
+    i3d++;
+    if ( i3d%2 == 0){
+    btn3d.style.background = "orange";
+    btn3d.style.color = "white";
+    btn3d.style.borderColor = "red";
+    btn3d.classList.remove('scale-up-center');
+} else if( i3d%2 == !0){
+    btn3d.style.background = "rgb(230, 204, 255)";
+    btn3d.style.color = "purple";
+    btn3d.style.borderColor = "rgb(145, 107, 242)";
+    btn3d.classList.add('scale-up-center');
+}
+});
+
+const btn4d = document.getElementById('btn4d');
+const btnOrange4d = btn4d.style.background = "orange";
+let i4d = 0;
+
+btn4d.addEventListener("click", function onClick(){
+    i4d++;
+    if ( i4d%2 == 0){
+    btn4d.style.background = "orange";
+    btn4d.style.color = "white";
+    btn4d.style.borderColor = "red";
+    btn4d.classList.remove('scale-up-center');
+} else if( i4d%2 == !0){
+    btn4d.style.background = "rgb(230, 204, 255)";
+    btn4d.style.color = "purple";
+    btn4d.style.borderColor = "rgb(145, 107, 242)";
+    btn4d.classList.add('scale-up-center');
+}
+});
+
+const btn5d = document.getElementById('btn5d');
+const btnOrange5d = btn5d.style.background = "orange";
+let i5d = 0;
+
+btn5d.addEventListener("click", function onClick(){
+    i5d++;
+    if ( i5d%2 == 0){
+    btn5d.style.background = "orange";
+    btn5d.style.color = "white";
+    btn5d.style.borderColor = "red";
+    btn5d.classList.remove('scale-up-center');
+} else if( i5d%2 == !0){
+    btn5d.style.background = "rgb(230, 204, 255)";
+    btn5d.style.color = "purple";
+    btn5d.style.borderColor = "rgb(145, 107, 242)";
+    btn5d.classList.add('scale-up-center');
+}
+});
+
+const btn6d = document.getElementById('btn6d');
+const btnOrange6d = btn6d.style.background = "orange";
+let i6d = 0;
+
+btn6d.addEventListener("click", function onClick(){
+    i6d++;
+    if ( i6d%2 == 0){
+    btn6d.style.background = "orange";
+    btn6d.style.color = "white";
+    btn6d.style.borderColor = "red";
+    btn6d.classList.remove('scale-up-center');
+} else if( i6d%2 == !0){
+    btn6d.style.background = "rgb(230, 204, 255)";
+    btn6d.style.color = "purple";
+    btn6d.style.borderColor = "rgb(145, 107, 242)";
+    btn6d.classList.add('scale-up-center');
+}
+});
